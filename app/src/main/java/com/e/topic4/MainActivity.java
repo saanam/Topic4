22bb644448638c50dcee241eed6c74b4eac92b08
sanam <shrestha722@gmail.com>
2019-11-21 08:23:27 +0545

    Hass map

--- app/src/main/java/com/e/topic4/MainActivity.java
+++ app/src/main/java/com/e/topic4/MainActivity.java
@@ -3,27 +3,51 @@ package com.e.topic4;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
     public static String countries[] ={
-            "Nepal",
-            "China",
-            "India",
-            "Canada",
-            "US"
+            "Nepal", "Kathmandu",
+            "China", "Beijing",
+            "India", "Delhi",
+            "Canada", "Ottawa",
+            "US", "Washing, D.C"
     };
+
+    private Map<String,String> dictonary;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ListView lstCountries = findViewById(R.id.lstcountries);
 
-        ArrayAdapter countryadapater = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, countries);
+        dictonary = new HashMap<>();
+        for(int i=0; i<countries.length; i+=2)
+        {
+            dictonary.put(countries[i],countries[i+1]);
+        }
+
+        ArrayAdapter countryadapater = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, new ArrayList<String>(dictonary.keySet()));
 
         lstCountries.setAdapter(countryadapater);
+
+        lstCountries.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String country = parent.getItemAtPosition(position).toString();
+                String capital = dictonary.get(country);
+                Toast.makeText(getApplicationContext(), capital, Toast.LENGTH_LONG).show();
+            }
+        });
 
     }
 }
